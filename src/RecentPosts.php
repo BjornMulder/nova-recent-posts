02f33f5fea44c2bf903579716e9c6871a162be3f
Matteo Mangoni <matt.mangoni@gmail.com>
2018-08-23 13:01:28 +0200

    initial commit

---
+++ src/RecentPosts.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Mattmangoni\RecentPosts;
+
+use Laravel\Nova\Card;
+
+class RecentPosts extends Card
+{
+    /**
+     * The width of the card (1/3, 1/2, or full).
+     *
+     * @var string
+     */
+    public $width = '1/3';
+
+    /**
+     * Get the component name for the element.
+     *
+     * @return string
+     */
+    public function component()
+    {
+        return 'recent-posts';
+    }
+}
